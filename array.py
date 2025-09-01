11773df43c0b01f33e2c2cb3c716924303344ee3
Amanda <80905646+amanda-m-sa@users.noreply.github.com>
2025-09-01 20:15:24 -0300

    array.py

---
+++ array.py
@@ -0,0 +1,11 @@
+# Atividade ARRAY
+listStudants = ['David', 'Kayk', 'Itamar', 'Mauricio', 'Ariel']
+
+#1º elemento do array
+print("1º nome da lista de alunos:", listStudants[0]) 
+
+#3º elemento do array
+print("3º nome da lista de alunos:", listStudants[2])
+
+#Último elemento do array
+print("Último nome da lista de alunos:", listStudants[-1])
